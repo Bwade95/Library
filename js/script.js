37bd3e8fba41e20497a2ad0db1011823e87d919a
Brandon Wade <bwade.95@hotmail.com>
2020-11-20 21:44:56 +0000

    added comments for createBook() function

--- js/script.js
+++ js/script.js
@@ -27,19 +27,24 @@ function createBook(item) {
     const library = document.createElement('div');
     library.classList.add('book-container');
     
+    // Stores styling and content of entire book
     const bookDiv = document.createElement('div');
     bookDiv.classList.add('book');
     bookDiv.setAttribute('id', myLibrary.indexOf(item));
 
+    // Stores styling and content of book title
     const titleDiv = document.createElement('div');
     titleDiv.classList.add('title');
 
+    // Stores styling and content of book author
     const authorDiv = document.createElement('div');
     authorDiv.classList.add('author');
 
+    // Stores styling and content of remove button
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('remove-button');
 
+    // Stores styling and content of book completion checker
     const bookRead = document.createElement('input');
     bookRead.classList.add('book-read');
     bookRead.setAttribute('type', 'radio');
