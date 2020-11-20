57d31ad186bc8b44ff726dacdee1fc631c0b1a0b
Brandon Wade <bwade.95@hotmail.com>
2020-11-20 21:34:06 +0000

    started creating book info card function

--- js/script.js
+++ js/script.js
@@ -18,8 +18,31 @@ let myLibrary = [];
 function addBookToLibrary() {
     let newBook = new Book(title, author);
     myLibrary.push(newBook);
-    console.log(myLibrary);
 }
 
+// Creates web-card storing book information
+function createBook(item) {
+
+    // div for card holding book info
+    const library = document.createElement('div');
+    library.classList.add('book-container');
+    
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add('title');
+
+    const authorDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-button');
+
+    const bookRead = document.createElement('input');
+    bookRead.classList.add('book-read');
+    bookRead.setAttribute('type', 'radio');
+}
 
 
