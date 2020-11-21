04b99bc0f9351676164d7ea7fc5f8dc007dbcd7a
Brandon Wade <bwade.95@hotmail.com>
2020-11-21 20:43:34 +0000

    create book almost working, but book info disappears as quickly as it appears

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.onclick = function(event) {
 }
 
 class Book {
-    constructor(title, author) {
+    constructor() {
         this.title = form.title.value;
         this.author = form.author.value;
     }
@@ -22,10 +22,11 @@ function addBookToLibrary() {
     newBook = new Book(title, author);
     myLibrary.push(newBook);
     render();
+    form.reset();
 }
 
 function render() {
-    const display = document.getElementById('book-container');
+    const display = document.getElementById('library-catalogue');
     const books = document.querySelectorAll('.book');
     books.forEach(book => display.removeChild(book));
 
@@ -36,38 +37,28 @@ function render() {
 
 // Creates web-card storing book information
 function createBook(item) {
-
     // div for card holding book info
-    const libraryDiv = document.createElement('div');
-    libraryDiv.classList.add('book-container');
-    library = document.querySelector('.book-container');
+    library = document.querySelector('#library-catalogue');
     
     // Stores styling and content of entire book
     const bookDiv = document.createElement('div');
     bookDiv.classList.add('book');
     bookDiv.setAttribute('id', myLibrary.indexOf(item));
-    library.appendChild(bookDiv);
 
     // Stores styling and content of book title
     const titleDiv = document.createElement('div');
+    titleDiv.textContent = item.title;
     titleDiv.classList.add('title');
     bookDiv.appendChild(titleDiv);
+    console.log(titleDiv.textContent);
 
     // Stores styling and content of book author
     const authorDiv = document.createElement('div');
+    authorDiv.textContent = item.author;
     authorDiv.classList.add('author');
     bookDiv.appendChild(authorDiv);
 
-    // Stores styling and content of remove button
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove-button');
-    bookDiv.appendChild(removeBtn);
-
-    // Stores styling and content of book completion checker
-    const bookRead = document.createElement('input');
-    bookRead.classList.add('book-read');
-    bookRead.setAttribute('type', 'radio');
-    bookDiv.appendChild(bookRead);
+    library.appendChild(bookDiv);
 }
 
 
